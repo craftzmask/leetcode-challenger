2bb4d10661588374ef5c93f79da584fccaf9f049
Khanh Chung <khanh.d.chung@gmail.com>
2023-09-08 11:16:27 -0700

    217. Contains Duplicate

---
+++ algorithms/contains_duplicate.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  nums.sort();
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] === nums[i]) return true;
+  }
+  return false;
+};
